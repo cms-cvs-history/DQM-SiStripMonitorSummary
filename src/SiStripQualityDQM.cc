2fa812762dbf36356902adfdc6aa5add681a3b7e
cmsbuild <>
2013-03-01 08:35:40 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- SiStripMonitorSummary-CMSSW_5_3_10_patch1'.
    Sprout from master 2012-08-10 09:38:16 UTC Mia Tosi <sha1-49252ea2ee74f24a84520d3c9546454852bedc56@cern.ch> 'fix crash w/ Profile'
    Cherrypick from master 2013-03-01 08:35:37 UTC Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch> 'html templates added to the package. Output redirected on different files to avoid log file parsing. Clean up of the scripts. RunInfo monitoring scripts and cfg added to the package':
        bin/BuildFile.xml
        bin/makePlots.cc
        bin/makeTKTrend.cc
        data/template_index_foot.html
        data/template_index_header.html
        scripts/MonitorDB_NewDirStructure_KeepTagLinks_generic.sh
        scripts/Monitor_NoiseRatios.sh
        scripts/Monitor_RunInfo.sh
        scripts/afstokenchecker.sh
        scripts/getOfflineDQMData.sh
        scripts/iov_list_tag.py
        test/DBReader_conddbmonitoring_generic_cfg.py
        test/RunInfo_conddbmonitoring_cfg.py
        test/SiStripCorrelateNoise_conddbmonitoring_cfg.py
    Cherrypick from DQM-SiStripMonitorSummary-B53x-61x 2013-03-01 08:35:38 UTC Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch> 'html templates added to the package. Output redirected on different files to avoid log file parsing. Clean up of the scripts. RunInfo monitoring scripts and cfg added to the package':
        src/SiStripBaseCondObjDQM.cc
        src/SiStripCablingDQM.cc
        src/SiStripQualityDQM.cc
    Delete:
        doc/SiStripMonitorSummary.doc
        test/CondDBMonitoring/MonitorDB_NewDirStructure.sh
        test/CondDBMonitoring/MonitorDB_OldDirStructure.sh
        test/CondDBMonitoring/Monitor_NoiseRatios.sh
        test/CondDBMonitoring/Monitor_RunInfo.sh
        test/CondDBMonitoring/cronjob.sh
        test/CondDBMonitoring/cronjob.txt
        test/CondDBMonitoring/getOfflineDQMData.sh
        test/CondDBMonitoring/makePlots.cc
        test/CondDBMonitoring/makeTKTrend.cc
        test/CondDBMonitoring/template_DBReaderRunInfo_cfg.py
        test/CondDBMonitoring/template_DBReader_cfg.py
        test/CondDBMonitoring/template_SiStripCorrelateNoise_cfg.py

--- src/SiStripQualityDQM.cc
+++ src/SiStripQualityDQM.cc
@@ -248,7 +248,7 @@ void SiStripQualityDQM::fillGrandSummaryMEs(){
   for(;idet!=detids.end();++idet){
     ss << "detid " << (*idet) << " IsModuleUsable " << qualityHandle_->IsModuleUsable((*idet)) << "\n";
   }
-  LogDebug("SiStripQualityStatistics") << ss.str() << std::endl;
+  LogDebug("SiStripQualityDQM") << ss.str() << std::endl;
 
 
   std::vector<SiStripQuality::BadComponent> BC = qualityHandle_->getBadComponentList();
@@ -444,7 +444,7 @@ void SiStripQualityDQM::fillGrandSummaryMEs(){
     ss << "\nTEC- Disk " << i-9 << " :" << ssV[3][i].str();
 
 
-  edm::LogInfo("SiStripQualityStatistics") << ss.str() << std::endl;
+  edm::LogInfo("SiStripQualityDQM") << ss.str() << std::endl;
 
   for (int i=0; i<4; i++){
     TCanvas c1("c1");
